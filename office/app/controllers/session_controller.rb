a550e5756ec19803498c04b4092ba7ac770a533b
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-04-10 18:51:31 -0400

    Module7: For revision

--- office/app/controllers/session_controller.rb
+++ office/app/controllers/session_controller.rb
@@ -5,10 +5,17 @@ class SessionController < ApplicationController
   def login
     @user = User.authenticate(params[:name][0], params[:password][0])
     if @user
+      flash[:notice] = "Welcome #{@user.name}"
       session[:user] = @user.id
       redirect_to root_path
     else
+      flash[:notice] = 'Error! Please type a valid Username and Password'
       render 'index'
     end
+  end
+
+  def logout
+    session[:user] = nil
+    redirect_to root_path
   end
 end
--- office/config/routes.rb
+++ office/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   get 'login' => 'session#index'
   post 'login' => 'session#login'
+  post 'logout' => 'session#logout'
   root 'welcome#index'
   resources :employees
   resources :companies
