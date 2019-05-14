42a6fb9bded285dbfffb1530f07e05dc5b41786d
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-05-14 09:51:32 -0400

    Complete new start for  9th assingment

--- myfirstapp/test/controllers/survey_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SurveyControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get survey_index_url
-    assert_response :success
-  end
-
-end
--- office/app/models/employee.rb
+++
@@ -1,5 +0,0 @@
-# Model class code
-class Employee < ApplicationRecord
-  belongs_to :company
-  validates :name, presence: :true
-end
